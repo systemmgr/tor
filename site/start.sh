4828667cb9cfa720dc5a602dcd5942074c487b17
casjay <git-admin@casjaysdev.com>
2020-08-16 01:41:34 -0400

    Update start.sh

--- site/start.sh
+++ site/start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export TOR_SITE_DIR="${TOR_SITE_DIR:-/usr/local/share/tor}"
-export TOR_PUBLIC_DIR="${TOR_PUBLIC_DIR:-TOR_SITE_DIR/public}"
-cd $TOR_PUBLIC_DIR || exit 1
+export TOR_PUBLIC_DIR="${TOR_PUBLIC_DIR:-$TOR_SITE_DIR/public}"
+cd "$TOR_PUBLIC_DIR" || exit 1
 
 lite-server -c "$TOR_SITE_DIR/bs-config.json" || \
 python3 -m http.server --cgi 8082 || \
