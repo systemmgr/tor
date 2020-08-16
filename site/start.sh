1d1cee8dbaed036c5ebe3408710989bf75171b58
casjay <git-admin@casjaysdev.com>
2020-08-16 01:46:47 -0400

    Update start.sh

--- site/start.sh
+++ site/start.sh
@@ -2,9 +2,7 @@
 
 export TOR_SITE_DIR="${TOR_SITE_DIR:-/usr/local/share/tor}"
 export TOR_PUBLIC_DIR="${TOR_PUBLIC_DIR:-$TOR_SITE_DIR/public}"
-cd "$TOR_PUBLIC_DIR" || exit 1
 
-lite-server -c "$TOR_SITE_DIR/bs-config.json" || \
-python3 -m http.server --cgi 8082 || \
-python2 -m http.server --cgi --directory "$TOR_PUBLIC_DIR" 8082 || \
-echo "Failed to start the tor site server"
+python3 -m http.server --cgi --bind 127.0.0.1 8082 || \
+python2 -m http.server --cgi --bind 127.0.0.1 8082 || \
+echo "Failed to start the tor site server" 
