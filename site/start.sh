7f1fe2d6aa4cd7588583c475535e52f9127ea5e8
casjay <git-admin@casjaysdev.com>
2020-08-16 01:51:49 -0400

    Update start.sh

--- site/start.sh
+++ site/start.sh
@@ -5,4 +5,5 @@ export TOR_PUBLIC_DIR="${TOR_PUBLIC_DIR:-$TOR_SITE_DIR/public}"
 
 python3 -m http.server --cgi --bind 127.0.0.1 8082 || \
 python2 -m http.server --cgi --bind 127.0.0.1 8082 || \
+python -m SimpleHTTPServer 8082 || \
 echo "Failed to start the tor site server" 
