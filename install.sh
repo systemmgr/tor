193d67633541a0a17b8e9ab84182c01f8a1b87b6
casjay <git-admin@casjaysdev.com>
2020-08-26 22:00:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,28 +47,21 @@ PLUGNAME=""
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# git repos
-
-PLUGINREPO=""
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # Version
 
-APPVERSION="$(curl -LSs ${DOTFILESREPO:-https://github.com/casjay-dotfiles}/$APPNAME/raw/master/version.txt)"
+APPVERSION="$(curl -LSs ${SYSTEMMGRREPO:-https://github.com/systemmgr}/$APPNAME/raw/master/version.txt)"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # if installing system wide - change to system_installdirs
 
-system_installdirs
+systemmgr_installer
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Set options
 
-APPDIR="$CONF/$APPNAME"
-PLUGDIR="$SHARE/$APPNAME/${PLUGNAME:-plugins}"
+APPDIR="$HOMEDIR/$APPNAME"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -146,29 +139,10 @@ failexitcode
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Plugins
-
-if [ "$PLUGNAME" != "" ]; then
-  if [ -d "$PLUGDIR"/.git ]; then
-    execute \
-      "git_update $PLUGDIR" \
-      "Updating $PLUGNAME"
-  else
-    execute \
-      "git_clone $PLUGINREPO $PLUGDIR" \
-      "Installing $PLUGNAME"
-  fi
-fi
-
-# exit on fail
-failexitcode
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # run post install scripts
 
 run_postinst() {
-  run_postinst_global
+  run_postinst_systemgr
   devnull systemctl stop tor tor-site.service
   mkdir -p /usr/local/share/tor/public
   cp_rf "$APPDIR/torrc" /etc/tor/torrc
@@ -194,7 +168,7 @@ execute \
 
 # create version file
 
-install_version
+install_systemmgr_version
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
