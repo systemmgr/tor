8b3b88b87d31d6f3e5cd963cb6d99c56ba993477
casjay <git-admin@casjaysdev.com>
2020-08-28 21:25:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-APPNAME="$(basename $0)"
+APPNAME="tor"
 USER="${SUDO_USER:-${USER}}"
 HOME="${USER_HOME:-${HOME}}"
 
@@ -8,7 +8,7 @@ HOME="${USER_HOME:-${HOME}}"
 # @Author          : Jason
 # @Contact         : casjaysdev@casjay.net
 # @File            : install.sh
-# @Created         : Wed, Aug 09, 2020, 02:00 EST
+# @Created         : Fr, Aug 28, 2020, 00:00 EST
 # @License         : WTFPL
 # @Copyright       : Copyright (c) CasjaysDev
 # @Description     : installer script for tor
@@ -33,6 +33,9 @@ else
 fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+system_installdirs
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Make sure the scripts repo is installed
 
@@ -41,27 +44,17 @@ scripts_check
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # Defaults
-
-APPNAME="tor"
-PLUGNAME=""
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Version
-
-APPVERSION="$(curl -LSs ${SYSTEMMGRREPO:-https://github.com/systemmgr}/$APPNAME/raw/master/version.txt)"
+APPNAME="${APPNAME:-tor}"
+APPDIR="/usr/local/etc/$APPNAME"
+REPO="${SYSTEMMGRREPO:-https://github.com/systemmgr}/${APPNAME}"
+REPORAW="${REPORAW:-$REPO/raw}"
+APPVERSION="$(curl -LSs $REPORAW/master/version.txt)"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# if installing system wide - change to system_installdirs
+# dfmgr_install fontmgr_install systemmgr_install pkmgr_install systemmgr_install thememgr_install wallpapermgr_install
 
-systemmgr_installer
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Set options
-
-APPDIR="$HOMEDIR/$APPNAME"
+systemmgr_install
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -71,46 +64,12 @@ show_optvars "$@"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# Requires root - no point in continuing
-
-sudoreq # sudo required
-#sudorun  # sudo optional
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 # end with a space
 
 APP="$APPNAME nyx torsocks "
-PERL=""
-PYTH=""
-PIPS=""
-CPAN=""
-GEMS=""
 
 # install packages - useful for package that have the same name on all oses
 install_packages $APP
-
-# install required packages using file
-install_required $APP
-
-# check for perl modules and install using system package manager
-install_perl $PERL
-
-# check for python modules and install using system package manager
-install_python $PYTH
-
-# check for pip binaries and install using python package manager
-install_pip $PIPS
-
-# check for cpan binaries and install using perl package manager
-install_cpan $CPAN
-
-# check for ruby binaries and install using ruby package manager
-install_gem $GEMS
-
-# Other dependencies
-dotfilesreq
-dotfilesreqadmin
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -125,13 +84,13 @@ ensure_perms
 
 if [ -d "$APPDIR/.git" ]; then
   execute \
-    "git_update $APPDIR" \
-    "Updating $APPNAME configurations"
+  "git_update $APPDIR" \
+  "Updating $APPNAME configurations"
 else
   execute \
-    "backupapp && \
-    git_clone -q $REPO/$APPNAME $APPDIR" \
-    "Installing $APPNAME configurations"
+  "backupapp && \
+        git_clone -q $REPO/$APPNAME $APPDIR" \
+  "Installing $APPNAME configurations"
 fi
 
 # exit on fail
@@ -142,7 +101,7 @@ failexitcode
 # run post install scripts
 
 run_postinst() {
-  run_postinst_systemgr
+  systemmgr_run_post
   devnull systemctl stop tor tor-site.service
   mkdir -p /usr/local/share/tor/public
   cp_rf "$APPDIR/torrc" /etc/tor/torrc
@@ -157,18 +116,17 @@ run_postinst() {
   devnull chmod go-rwx /var/lib/tor/hidden_service
   devnull systemctl restart tor tor-site.service && sleep 30
   replace "/usr/local/share/tor/public" MYHOSTNAME "$(hostname)"
-
 }
 
 execute \
-  "run_postinst" \
-  "Running post install scripts"
+"run_postinst" \
+"Running post install scripts"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # create version file
 
-install_systemmgr_version
+systemmgr_install_version
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
