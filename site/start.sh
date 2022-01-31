b0fbcf245b44feba275751da4c5bfa85ab9bad88
Jason <git-admin@casjaysdev.com>
2022-01-30 22:40:01 -0500

    🗃️ modified: site/start.sh 🗃️

--- site/start.sh
+++ site/start.sh
@@ -3,7 +3,7 @@
 export TOR_SITE_DIR="${TOR_SITE_DIR:-/usr/local/share/tor}"
 export TOR_PUBLIC_DIR="${TOR_PUBLIC_DIR:-$TOR_SITE_DIR/public}"
 
-python3 -m http.server --cgi --bind 127.0.0.1 8082 || \
-python2 -m http.server --cgi --bind 127.0.0.1 8082 || \
-python -m SimpleHTTPServer 8082 || \
-echo "Failed to start the tor site server" 
+python3 -m http.server --cgi --bind 127.0.0.2 8082 ||
+  python2 -m http.server --cgi --bind 127.0.0.2 8082 ||
+  python -m SimpleHTTPServer 8082 ||
+  echo "Failed to start the tor site server"
